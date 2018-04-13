aaf7deeb28994b2f61ade428c144eed5bc0237f8
chrisxjacobi <chrisxjacobi@gmail.com>
2018-04-12 22:05:47 -0500

    loaded http server, configured to integrate socket.io for client <-> server connections, used built in events (connect/disconnect)

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
-// added new chat application folder,
+// loaded http server, configured to integrate socket.io for client <-> server connections, used built in events (connect/disconnect)
 
+const path = require('path');
+const http = require('http');
 const express = require('express');
-var app = express();
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
 
 
 
+
+
+// web sockets are persistent. communication is open for as long as server/client likes.
 
 
 // console.log(__dirname + '/../public');
