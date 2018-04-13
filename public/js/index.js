30187aaf669827a66b61e3406648535a78d8c2e4
chrisxjacobi <chrisxjacobi@gmail.com>
2018-04-13 18:40:37 -0500

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'sunny@example.com',
-    text: 'No way!'
-  });
 });
 
 socket.on('disconnect', function () {
