c9f421ecceb2e1f06ccb888ec5849eb84cf53574
chrisxjacobi <chrisxjacobi@gmail.com>
2018-04-13 22:00:16 -0500

    created generateMessage utility

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -16,29 +17,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit to new user from admin, text: 'welcome to the chat app'
-  //socket.broadcase.emit to everyone, from admin, text: 'new user joined'
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
 
-  socket.emit('newMessage', {
-    from: 'admin',
-    text: 'welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
-
-  socket.broadcast.emit('newMessage', {
-    from: 'admin',
-    text: 'new user joined',
-    createdAt: new Date().getTime()
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
---
+++ server/utils/message.test.js
@@ -0,0 +1,16 @@
+const expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate the correct message object', () => {
+    var from = 'chris';
+    var text = 'some message';
+    var message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+
+  
+});
