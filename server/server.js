861673ee9354c3c004d3d8ad1ae75d3c4469efa8
chrisxjacobi <chrisxjacobi@gmail.com>
2018-04-22 17:21:04 -0500

    added string validation and test cases, also deparam library to set routes for name and chat room added at join homepage

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -18,6 +19,14 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room name are required.')
+    };
+
+    callback()
+  });
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
